6162b2afffcb8c59d50de8dbd4b283d0d61a7bb6
marianashy <marjanka77777@gmail.com>
2024-05-04 23:57:22 +0200

    search form

--- newsapp/src/components/search/index.tsx
+++ newsapp/src/components/search/index.tsx
@@ -1,5 +1,24 @@
+import { SearchContext } from '../../utils/context/searchContext';
+import React, { useContext } from 'react';
+
+
+
 function SEARCH() {
-	return (<div>SEARCH</div>);
+
+	const { searchRequest, setSearchRequest } = useContext(SearchContext);
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	  setSearchRequest(e.target.value);
+	};
+
+console.log(searchRequest)
+	return (
+		<form>
+			<input type="text" name="searchRequest" value={searchRequest} onChange={handleChange}/>
+			<button type="submit">Search</button>
+			<p>searchRequest</p>
+	   </form>
+);
 }
 
 export default SEARCH;
